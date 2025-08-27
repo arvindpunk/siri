8f1678adda9df71cb2ecdb88f6b471268db92ecc
Arvind PJ <arvindpunk@gmail.com>
2025-08-27 23:55:20 +0530

    feat: add random reply chance

--- lib/siri/consumer.ex
+++ lib/siri/consumer.ex
@@ -11,8 +11,13 @@ defmodule Siri.Consumer do
 
   @spec handle_event({atom(), Nostrum.Struct.Message.t(), any()}) :: any()
   def handle_event({:MESSAGE_CREATE, msg, _ws_state}) do
-    if Enum.any?(msg.mentions, fn user -> user.id == @bot_id end) do
-      referenced_message = Map.get(msg.referenced_message, :content)
+    should_respond_without_mentioned =
+      :rand.uniform() < 0.15 and msg.author.id != @bot_id and
+        String.length(msg.content) > 50
+
+    if should_respond_without_mentioned or
+         Enum.any?(msg.mentions, fn user -> user.id == @bot_id end) do
+      referenced_message = Map.get(msg.referenced_message || %{}, :content)
       currrent_message = msg.content
 
       messages =
@@ -38,6 +43,7 @@ defmodule Siri.Consumer do
         )
 
       Message.create(msg.channel_id,
+        # content: "[debug stuff #{length(messages)}] " <> response.content
         content: response.content
       )
     else
